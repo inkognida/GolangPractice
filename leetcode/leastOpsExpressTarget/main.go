fb6b51de3dc79589c87d3354f885782176ea9137
Hiedi Ardella <hardella@mi-p3.msk.21-school.ru>
2023-03-06 23:32:36 +0300

    leetcode 964 started

---
+++ leetcode/leastOpsExpressTarget/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func leastOpsExpressTarget(x int, target int) int {
+	r := 0
+
+	/*
+		if even -> do something
+		if odd -> do something
+	*/
+
+	return r
+}
+
+func main() {
+	fmt.Println(leastOpsExpressTarget(3, 19))
+}

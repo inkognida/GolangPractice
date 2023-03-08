e1ee2079b08e8360d68fd23134ef07b954740147
Hiedi Ardella <hardella@mi-p5.msk.21-school.ru>
2023-03-08 23:46:36 +0300

    leetcode practice

---
+++ leetcode/binaryTreeCameras/main.go
@@ -0,0 +1,17 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minCameraCover(root *TreeNode) int {
+	var r int
+
+	return r
+}
+
+func main() {
+
+}

3bc9519051cd45f477849d7f1f2b2b476a8c838d
Hiedi Ardella <hardella@mi-p5.msk.21-school.ru>
2023-04-06 17:11:29 +0300

    sad

--- yaIntern/internWeek/towers/main.go
+++ yaIntern/internWeek/towers/main.go
@@ -15,91 +15,52 @@ func SplitBySeparator(s string, sep rune) []string {
 	})
 }
 
-func lds(arr []int) []int {
-	n := len(arr)
-	lds := make([]int, n)
-	for i := range lds {
-		lds[i] = 1
+func reverse(ss []string) {
+	last := len(ss) - 1
+	for i := 0; i < len(ss)/2; i++ {
+		ss[i], ss[last-i] = ss[last-i], ss[i]
 	}
-	for i := 1; i < n; i++ {
-		for j := 0; j < i; j++ {
-			if arr[j] > arr[i] && lds[i] < lds[j]+1 {
-				lds[i] = lds[j] + 1
-			}
-		}
-	}
-	max := 0
-	end := 0
-	for i := 0; i < n; i++ {
-		if max < lds[i] {
-			max = lds[i]
-			end = i
-		}
-	}
-	result := make([]int, max)
-	result[max-1] = arr[end]
-	for i := max - 2; i >= 0; i-- {
-		for j := end - 1; j >= 0; j-- {
-			if arr[j] > arr[end] && lds[j] == i+1 {
-				result[i] = arr[j]
-				end = j
-				break
-			}
-		}
-	}
-	return result
 }
 
 func main() {
 	var r *bufio.Reader
-
-	if file, err := os.Open("input.txt"); err != nil {
-		r = bufio.NewReader(os.Stdin)
-	} else {
-		r = bufio.NewReader(file)
-	}
-
-	dsLine, _ := r.ReadString('\n')
-	d := SplitBySeparator(strings.TrimSpace(dsLine), ' ')
-	n, _ := strconv.Atoi(d[0])
-	m, _ := strconv.Atoi(d[1])
-
+	file, _ := os.Open("input.txt")
+	r = bufio.NewReader(file)
+	r.ReadString('\n')
 	plsWline, _ := r.ReadString('\n')
 	plsW := SplitBySeparator(strings.TrimSpace(plsWline), ' ')
-	pls := make([]int, n)
-	for i, v := range plsW {
+	reverse(plsW)
+	platforms := make(map[int]struct{}, 0)
+	pls := make([]int, 0)
+	max := 0
+	for _, v := range plsW {
 		tmp, _ := strconv.Atoi(v)
-		pls[i] = tmp
-	}
-
-	gwl, _ := r.ReadString('\n')
-	gw := SplitBySeparator(strings.TrimSpace(gwl), ' ')
-	g := make([]int, m)
-	for i, v := range gw {
-		tmp, _ := strconv.Atoi(v)
-		g[i] = tmp
-	}
-
-	platforms := lds(pls)
-	sort.Slice(g, func(i, j int) bool {
-		return g[i] < g[j] // Reverse the order
-	})
-
-	for i := 0; i < len(platforms)-1; i++ {
-		platforms[i] = platforms[i] - platforms[i+1]
-	}
-	platforms[len(platforms)-1] = platforms[len(platforms)-1] - 1
-
-	c := 0
-Main:
-	for _, v := range g {
-		for _, p := range platforms {
-			if v <= p {
-				c++
-				continue Main
-			}
+		if _, ok := platforms[tmp]; !ok && tmp > max {
+			platforms[tmp] = struct{}{}
+			pls = append(pls, tmp-max)
+			max = tmp
 		}
 	}
+	sort.Ints(pls)
+	gwl, _ := r.ReadString('\n')
+	gw := SplitBySeparator(strings.TrimSpace(gwl), ' ')
 
+	l := len(pls)
+	c := 0
+	for _, v := range gw {
+		tmp, _ := strconv.Atoi(v)
+		if l > 0 {
+			for j, _ := range pls {
+				if tmp <= pls[j] {
+					c++
+					l--
+					pls[j] = 0
+					break
+				}
+			}
+		} else {
+			break
+		}
+	}
 	fmt.Println(c)
 }
